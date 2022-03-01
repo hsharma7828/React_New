55c5255bd1c77d3b8dcb329d1aa7432c705ef908
Himanshu Sharma <hsharma114@gmail.com>
2022-03-01 23:18:26 +0530

    Changes and mofication

---
+++ src/components/Expenses.js
@@ -0,0 +1,33 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Cards";
+import './Expenses.css'
+
+function Expenses(props) {
+    let expense = props.props
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={expense[0].title}
+        amount={expense[0].amount}
+        date={expense[0].date}
+      />
+      <ExpenseItem
+        title={expense[1].title}
+        amount={expense[1].amount}
+        date={expense[1].date}
+      />
+      <ExpenseItem
+        title={expense[2].title}
+        amount={expense[2].amount}
+        date={expense[2].date}
+      />
+      <ExpenseItem
+        title={expense[3].title}
+        amount={expense[3].amount}
+        date={expense[3].date}
+      />
+    </Card>
+  );
+}
+
+export default Expenses;
